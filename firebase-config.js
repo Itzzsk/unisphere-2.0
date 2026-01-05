981da41c00e07e64d242185000a25393530cafbf
skanda umesh <skandaumesh82@gmail.com>
2026-01-06 00:02:01 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 // firebase-config.js
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAn172ndihcZRqCBAxKYATkc3Hf_uNi4hw",
-    authDomain: "unisphere-35100.firebaseapp.com",
-    projectId: "unisphere-35100",
-    storageBucket: "unisphere-35100.firebasestorage.app",
-    messagingSenderId: "331319571025",
-    appId: "1:331319571025:web:f21eaacd723daff8533e70"
-  };
-  
-  // Export for use in HTML
-  window.FIREBASE_CONFIG = firebaseConfig;
-  
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Export for use in HTML
+window.FIREBASE_CONFIG = firebaseConfig;
